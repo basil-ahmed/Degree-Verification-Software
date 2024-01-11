b30d825b80b91760084059c7a31a8865424f8d41
basil-ahmed <bq2024@nyu.edu>
2024-01-12 03:21:21 +0500

    added vercel web analytics

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,9 @@ import Login from './login';
 import Signup from './signup';
 import Home from './home';
 import VerificationPage from './verificationPage';
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 const App = () => {
   const isAuthenticated = !!localStorage.getItem('token'); // Update this to your actual authentication check
